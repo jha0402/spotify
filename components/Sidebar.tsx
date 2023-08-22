972c4d58ccbd40fd5f29fb7a7172414b95c4f47c
jha0402 <67306416+jha0402@users.noreply.github.com>
2023-08-22 16:40:43 +0900

    Add home page screen components: sidebar, library, main

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface Sidebarprops {
     children: React.ReactNode;
@@ -10,8 +15,14 @@ const Sidebar: React.FC<Sidebarprops> = ({ children }) => {
     const pathname = usePathname();
     const routes = useMemo(
         () => [
-            { label: "Home", active: pathname !== "/search", href: "/" },
             {
+                icon: HiHome,
+                label: "Home",
+                active: pathname !== "/search",
+                href: "/",
+            },
+            {
+                icon: BiSearch,
                 label: "Search",
                 active: pathname === "/search",
                 href: "/search",
@@ -19,7 +30,25 @@ const Sidebar: React.FC<Sidebarprops> = ({ children }) => {
         ],
         []
     );
-    return <div>Sidebar</div>;
+    return (
+        <div className="flex h-full">
+            <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+                <Box>
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((item) => (
+                            <SidebarItem key={item.label} {...item} />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library />
+                </Box>
+            </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
+        </div>
+    );
 };
 
 export default Sidebar;
