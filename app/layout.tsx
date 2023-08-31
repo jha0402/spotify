42c4e20d2a618f6de0568053fa693a031821d7b5
jha0402 <67306416+jha0402@users.noreply.github.com>
2023-08-31 19:14:29 +0900

    prettier fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
-import Sidebar from "@/components/Sidebar";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Figtree } from "next/font/google";
+import Sidebar from '@/components/Sidebar';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+import SupbaseProvider from '@/providers/SupabaseProvider';
 
-const font = Figtree({ subsets: ["latin"] });
+const font = Figtree({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: "Spotify clone",
-    description: "Listen to music",
+  title: 'Spotify clone',
+  description: 'Listen to music',
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={font.className}>
-                <Sidebar>{children}</Sidebar>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupbaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupbaseProvider>
+      </body>
+    </html>
+  );
 }
